3b6b9ee034a88fc3889d0d2d5d6ca1c36518943a
IonelLupu <wolff_cristy@yahoo.com>
2020-04-01 12:32:08 +0300

    updated to Typetron@0.0.11

--- index.ts
+++ index.ts
@@ -4,8 +4,8 @@ import 'source-map-support/register';
 import '@Typetron/Support';
 import { AppConfig, Application } from '@Typetron/Framework';
 
-export const app = new Application(__dirname);
-
-app.startServer();
-const config = app.get(AppConfig);
-console.log(`Started server at: http://localhost:${config.port}`);
+Application.create(__dirname).then(app => {
+    app.startServer();
+    const config = app.get(AppConfig);
+    console.log(`Started server at: http://localhost:${config.port}`);
+});
--- test/TestCase.ts
+++ test/TestCase.ts
@@ -1,8 +1,13 @@
-import { app } from '../bootstrap/app';
 import { TestCase as BaseTestCase } from '@Typetron/Testing/TestCase';
+import { AppConfig, Application } from '@Typetron/Framework';
+import * as path from 'path';
 
 export class TestCase extends BaseTestCase {
-    bootstrapApp() {
+    async bootstrapApp() {
+        const app = await Application.create(path.join(__dirname, '..'));
+        app.startServer();
+        const config = app.get(AppConfig);
+        console.log(`Started server at: http://localhost:${config.port}`);
         this.app = app;
     }
 }
