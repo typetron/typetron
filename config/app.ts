a472565ed5cde53002b566964cd2d7ca82a4872e
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2020-12-16 23:55:20 +0200

    made static assets active by default on the public directory

--- config/app.ts
+++ config/app.ts
@@ -15,7 +15,7 @@ export default new AppConfig({
         RoutingProvider,
         DatabaseProvider,
     ],
-    // staticAssets: {
-    //     '*': ['public']
-    // }
+    staticAssets: {
+        '*': ['public']
+    }
 })
