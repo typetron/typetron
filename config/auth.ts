e2d4d1c373c9a335b3ccba3c8fab87fbb767caa7
Ionel Lupu <ionel.cristian.lupu@gmail.com>
2021-04-14 21:54:36 +0300

    updated env variables naming format to uppercase with underscore between words

--- config/auth.ts
+++ config/auth.ts
@@ -4,6 +4,6 @@ import { User } from 'App/Entities/User'
 export default new AuthConfig({
     duration: 3600,
     entity: User,
-    signature: '696?sX}Fqp,|3/$txG)5PJSVoAnCj|pW03ytqLbNX/9o,Q)5O>z4x3AV7aIDlbd',
+    signature: process.env.APP_SECRET,
     saltRounds: 12,
 })
--- config/database.ts
+++ config/database.ts
@@ -1,19 +1,19 @@
 import { DatabaseConfig } from '@Typetron/Framework'
-import { MysqlDriver, SqliteDriver } from '@Typetron/Database/Drivers'
+import { MysqlDriver, SqliteDriver } from '@Typetron/Database'
 
 export default new DatabaseConfig({
     entities: './Entities',
     synchronizeSchema: true,
     migrationsDirectory: 'migrations',
-    driver: process.env.databaseDriver ?? 'sqlite',
+    driver: process.env.DATABASE_DRIVER ?? 'sqlite',
 
     drivers: {
-        sqlite: () => new SqliteDriver(process.env.database ?? 'database.sqlite'),
+        sqlite: () => new SqliteDriver(process.env.DATABASE ?? 'database.sqlite'),
         mysql: () => new MysqlDriver({
-            host: process.env.databaseHost,
-            user: process.env.databaseUser,
-            password: process.env.databasePassword,
-            database: process.env.database,
+            host: process.env.DATABASE_HOST,
+            user: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE,
         }),
     }
 })
